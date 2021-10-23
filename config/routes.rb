2be712357ee5ff4dc5a444d8485d2bd67c4c5f80
Putzeys <gputzeys@gmail.com>
2021-10-23 17:19:25 -0300

    Homespage

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :homes
+  get 'homepage/new'
+  get 'homepage/create'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ test/controllers/homepage_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class HomepageControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get homepage_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get homepage_create_url
+    assert_response :success
+  end
+end
